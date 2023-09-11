62633483e4eecf0ff690b7aca9a2e606fbbee4fa
franco2730 <franco.rosales2130@gmail.com>
2023-09-11 16:48:01 -0300

    Repaso-PI

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const server = require("./src/app"); //Importamos el contenido de la variable app creada en el archivo app. (La podemos renombrar debido a que, al momento de exportarla de app.js, NO le aplicamos destructuring, si le hubiesemos aplicado, aca tambien lo tendriamos que hacer y con el nombre que lo exportamos anteriormente.) Como lo exportamos por default, tamos god.
 //const { conn } = require("./src/db");
+const { conn } = require ("./src/db");
 const PORT = 3001; //Creamos la variable del primer parametro del server.listen. Se recomienda utilizar para el back el 3001 (corroborar que no este ocupado).
 
 
--- api/src/db.js
+++ api/src/db.js
@@ -8,17 +8,10 @@ const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
 
 
 module.exports = { 
-    conn: sequelize, // Exportamos en forma de objeto ya que, desde este mismo archivo vamos a tener que exportar otras cosas, con lo cual, exportamos sequelize pero con la panabla conn que lo contenga, haciendo alución a CONEXIÓN.
+    conn: sequelize, 
  }
 
- //IMPORTANTE SABER:
- //Cuando fuimos a la documentación oficial de sequelize, vimos que tenemos que require sequelize y que luego vamos a crear una variable llamada sequelize que contiene una nueva instancia de esta con un link:
 
-//const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
 
-//Ahora tenemos que reemplazar este path por lo que nosotros necesitamos, pero antes, vamos a entender que significa: 
 
-//POSTGRES: Es el dialecto de la BD que vamos a utilizar. (en nuestro caso es postgres)
-//USER: Es el usuario que va a utilizar la BD (en nuestro caso es postgres)
-//PASS: Es la contraseña con la que vamos a ingresar a la BD. (en nuestro caso es postgres)
-//EXAMPLE.COM (en nuestro caso es localhost)
+
