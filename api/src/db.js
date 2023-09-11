59269980f358d607b5f156d42175848defaddedf
franco2730 <franco.rosales2130@gmail.com>
2023-09-11 09:37:42 -0300

    Repaso-PI

--- api/src/db.js
+++ api/src/db.js
@@ -1,3 +1,12 @@
 // Este archivo tendrá toda la responsabilidad de contener la configuración de la base de datos. Asi como App tiene la configuración de nuestro servidor.
 
+//En la documentación oficial podemos ver las formas que es recomendable trabajar e instalar dependencias.
+
 const { Sequelize } = require("sequelize"); 
+
+const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
+
+
+module.exports = { 
+    conn: sequelize, // Exportamos en forma de objeto ya que, desde este mismo archivo vamos a tener que exportar otras cosas, con lo cual, exportamos sequelize pero con la panabla conn que lo contenga, haciendo alución a CONEXIÓN.
+ }
